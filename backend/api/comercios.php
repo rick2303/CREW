d9fbad83501b81838fd9ef03ff398ac16cd56930
rick2303 <ricky95hnd@gmail.com>
2022-04-22 19:23:31 -0600

    Integracion API

--- backend/api/comercios.php
+++ backend/api/comercios.php
@@ -3,11 +3,6 @@
     include_once("../class/class-comercios.php");
     switch($_SERVER['REQUEST_METHOD']){
         case 'POST':
-            $_POST = json_decode(file_get_contents('php://input'), true);
-            $comercio = new Comercio($_POST['nombre'], $_POST['apellido'], $_POST['fechaNacimiento'], $_POST['pais']);
-            $comercio->guardarComercio();
-            $resultado["Mensaje"] = "Guardar el comercio, informacion: ". json_encode($_POST);
-            echo json_encode($resultado);
         break;
         case 'GET':
            if(isset($_GET['id'])){
@@ -18,17 +13,9 @@
                
          break;
         case 'PUT':
-            $_PUT = json_decode(file_get_contents('php://input'), true);
-            $comercio = new Comercio($_PUT['nombre'], $_PUT['apellido'], $_PUT['fechaNacimiento'], $_PUT['pais']);
-            $comercio->actualizarComercio($_GET['id']);
-            $resultado["mensaje"] = "Actualizar comercio con id: ". $_GET['id'].
-                                    ", Informacion a actualizar ". json_encode($_PUT);
-             echo json_encode($resultado);
         break;
         case 'DELETE':
-            Comercio::eliminarComercio($_GET['id']);
-            $resultado["mensaje"] = "Eliminar un comercio con id: ". $_GET['id'];
-            echo json_encode($resultado);
+        
         break;
     }
 
