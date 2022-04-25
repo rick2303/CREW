50397859b0da427db804b21f8ccf0367bf768dd6
rick2303 <ricky95hnd@gmail.com>
2022-04-25 12:19:44 -0600

    arreglo de bugs en controladorClientes

--- frontend/js/controladorClientes.js
+++ frontend/js/controladorClientes.js
@@ -1,167 +1,162 @@
 
 function barralateral() {
-
-    document.getElementById('barralateral').style.display = 'block';
-    document.getElementById('pantallanegra').style.display = 'block';
+   
+    document.getElementById('barralateral').style.display='block';
+    document.getElementById('pantallanegra').style.display='block';
 
 }
 function cerrar() {
-    document.getElementById('barralateral').style.display = 'none';
-    document.getElementById('pantallanegra').style.display = 'none';
-
+    document.getElementById('barralateral').style.display='none';
+    document.getElementById('pantallanegra').style.display='none';
+  
 }
-
 function cargarClientes() {
     axios({
-        method: 'GET',
-        url: '../../backend/api/clientes.php',
-        responseType: 'json',
-    }).then(res => {
-        clientes = res.data;
+        method:'GET',
+        url:'../../backend/api/clientes.php',
+        responseType:'json',
+    }).then(res=>{    
+        clientes=res.data;
         console.log(clientes)
-    }).catch(error => {
+    }).catch(error=>{
         console.log(error);
     });
 }
 
 cargarClientes();
- var indiceClienteLogueado = window.localStorage;
- var obteniendoIndice = window.localStorage;
-function agregaCliente() {
-    const clienteInfo = {
-        Nombre: document.getElementById('nombre').value,
-        CorreoElectronico: document.getElementById('correo').value,
-        Password: document.getElementById('password').value
+var indiceClienteLogueado = window.localStorage;
+var obteniendoIndice = window.localStorage;
+
+function  agregaCliente() {
+
+    const clienteInfo={
+        nombre:document.getElementById('nombre').value,
+        correoElectronico:document.getElementById('correo').value,
+        contrasena:document.getElementById('password').value,
+        verOrden :[],
     };
-
-    if (clientes != "") {
-        var rep = false;
+     if(clientes!=""){
+        var aux=false;
         for (let i = 0; i < clientes.length; i++) {
-            if (clientes[i].CorreoElectronico == clienteInfo.CorreoElectronico) {
+            if (clientes[i].correoElectronico==clienteInfo.correoElectronico) {
+                console.log("correo",clientes[i].correoElectronico)
                 console.log('repetido')
-                rep = true;
-                document.getElementById('modal-body').innerHTML = `
-        <h5 style="color: red" class="modal-title" id="exampleModalLabel">Al parecer usted ya tiene una cuenta registrada,por favor,intente de nuevo</h5>        
-        `
-                document.getElementById('cerrarRegistrar').innerHTML = `
-       
-        <button data-bs-toggle="modal" data-bs-target="#staticBackdropRegistrar" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        `
-                break;
-
-            } else {
-                rep = false
-
-            }
-
+                aux=true;
+                document.getElementById('modal-bopy').innerHTML =`
+                <h5 style="color: red" class="modal-title" id="exampleModalLabel">Al parecer usted ya tiene una cuenta registrada,por favor,intente de nuevo</h5>          
+                `
+                document.getElementById('cerrarRegistrar').innerHTML=`        
+                <button data-bs-toggle="modal" data-bs-target="#staticBackdropRegistrar" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                `
+                break;   
+            }else{
+            aux=false
+            } 
         }
-
     }
+    if(aux==false ||  clientes==""){
+        console.log(' agregado con exito');
 
-    if (rep == false || clientes == "") {
-        if (validacionCorreo(clienteInfo.CorreoElectronico)) {
-            console.log(' agregado con exito');
-            console.log(clientes);
-            clientes.push(clienteInfo);
-            localstorage.setItem('clientes', JSON.stringify(clientes));
-            console.log(clientes);
+        axios({
+            method:'POST',         
+            url:'../../backend/api/clientes.php',       
+            responseType:'json',
+            data:clienteInfo,
+          
+        }).then(res=>{    
+            cargarClientes();
+        }).catch(error=>{  
+        });
+        document.getElementById('nombre').value=null;
+        document.getElementById('correo').value=null;
+        document.getElementById('password').value=null
 
-            document.getElementById('nombre').value = null;
-            document.getElementById('correo').value = null;
-            document.getElementById('password').value = null
-
-            document.getElementById('modal-bopy').innerHTML = `
-<h5 style="color: green" class="modal-title" id="exampleModalLabel">Sus datos han sido enviados correctamente</h5>
-
-`
-            document.getElementById('cerrarRegistrar').innerHTML = `
-<button  type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-`
-
-        }
+        document.getElementById('modal-bopy').innerHTML =`
+        <h5 style="color: green" class="modal-title" id="exampleModalLabel">Sus datos han sido enviados correctamente</h5>
+        
+        `
+        document.getElementById('cerrarRegistrar').innerHTML=`
+        <button  type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        `
     }
 }
 
-    function loginCliente() {
-        for (let i = 0; i < clientes.length; i++) {
-            if (clientes[i].CorreoElectronico == document.getElementById('usuarioLogin').value && clientes[i].Password == document.getElementById('passwordLogin').value) {
-                console.log('encontrado');
-                localstorageLogueado.setItem('clientesLogueado', JSON.stringify(clientes[i]));
-                location.assign("clienteLogueado.html");
-                document.getElementById('usuarioLogin').value = null;
-                document.getElementById('passwordLogin').value = null;
+function loginCliente() {
+    console.log("si esta funcionando");console.log(clientes);
 
-
-                break;
-            } else {
-
-
-                console.log('no encontrado')
-            }
-
-        }
-
+    for (let i = 0; i < clientes.length; i++) {
+       if (clientes[i].correoElectronico==document.getElementById('usuarioLogin').value && clientes[i].contrasena==document.getElementById('passwordLogin').value) {
+           console.log('encontrado');
+           indiceClienteLogueado.setItem('indiceCliente', JSON.stringify(i));
+           location.assign("../../frontend/clientes/clienteLogueado.html");
+         
+           document.getElementById('usuarioLogin').value=null;
+           document.getElementById('passwordLogin').value=null;
+           break;
+       }else{
+        console.log('no encontrado')
+       }
+        
     }
+    
+}
 
-    var indiceCliente = JSON.parse(obteniendoIndice.getItem('indiceCliente'));
-    var indiceComercio = JSON.parse(obteniendoIndice.getItem('indiceComercio'));
+var indiceCliente=JSON.parse(obteniendoIndice.getItem('indiceCliente'));
+var indiceComercio = JSON.parse(obteniendoIndice.getItem('indiceComercio'));
 
-    var clienteLogueado;
+var clienteLogueado;
 
-    function inicio() {
-        document.getElementById('perfilFoto').innerHTML = `
+function inicio() {
+    document.getElementById('perfilFoto').innerHTML=`
     <div style="padding: 20px; " ><img style="height: 70px; border-radius: 8rem" src="../img/fotoperfil.jpg" alt=""></div>
     <div  style="padding: 20px; font-weight:bold;">${clienteLogueado.nombre}</div>  
            `;
+    
+}
 
-    }
-
-    function cargarInfoCliente() {
+function cargarInfoCliente() {
         axios({
-            method: 'GET',
-            url: '../../backend/api/clientes.php' + `?id=${indiceCliente}`,
-            responseType: 'json'
-        }).then(res => {
-
-            clienteLogueado = res.data;
-            console.log("prueba cliente", clienteLogueado.nombre);
+            method:'GET',
+            url:'../../backend/api/clientes.php'+ `?id=${indiceCliente}`,
+            responseType:'json'
+        }).then(res=>{
+            
+            clienteLogueado=res.data;
+            console.log("prueba cliente",clienteLogueado.nombre);
             inicio();
             existenciaPedidoActivo();
-        }).catch(error => {
+        }).catch(error=>{
         });
+     
+}
+cargarInfoCliente();
 
+function validacionCorreo(correo) {
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var valido = expReg.test(correo);
+    if(valido){
+        return true;
+    }else{
+        console.log('no valido');
+        return false;
     }
-    cargarInfoCliente();
-
-
-
-    function validacionCorreo(correo) {
-        var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-        var valido = expReg.test(correo);
-        if (valido) {
-            return true;
-        } else {
-            console.log('no valido');
-            return false;
-        }
-    }
-
-    var comercios = [];
-    var restaurantes = [];
-    function obtenerComercios() {
-
-        axios({
-            method: 'GET',
-            url: '../../backend/api/comercios.php',
-            responseType: 'json'
-        }).then(res => {
-            comercios = res.data;
-            console.log(comercios);
-        }).catch(error => {
-            console.log(error);
-        });
-        return comercios;
-    } obtenerComercios();
+}
+var comercios=[];
+var restaurantes = [];
+function obtenerComercios(){
+    
+    axios({
+        method:'GET',
+        url:'../../backend/api/comercios.php',
+        responseType:'json'
+    }).then(res=>{
+        comercios=res.data;
+        console.log(comercios);
+    }).catch(error=>{
+        console.log(error);
+    });
+    return comercios;
+} obtenerComercios();
 
 function ordenes(){}
 
